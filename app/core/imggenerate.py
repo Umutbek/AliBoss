c02bae6344179ae5397efafedbd86c5ee7f5b125
Umutbek <karimovumutbek@gmail.com>
2022-03-12 09:39:14 +0600

    Img generate fixed

--- app/core/imggenerate.py
+++ app/core/imggenerate.py
@@ -4,6 +4,5 @@ import os
 def all_image_file_path(instance, filename):
     """Generate file path for all image"""
     ext = filename.split('.')[-1]
-    print("Instance", instance)
     filename = f'{uuid.uuid4()}.{ext}'
     return os.path.join('uploads/', filename)
