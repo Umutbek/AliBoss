95abde5aa1b8e1baf8ba4dfd3d94f6dff46e2d36
root <root@aliboss.store>
2022-12-18 13:51:37 +0600

    servers test

--- app/core/views.py
+++ app/core/views.py
@@ -42,8 +42,8 @@ class ItemViewSet(viewsets.ModelViewSet):
 
 #    def list(self, request, *args, **kwargs):
 #        queryset = models.Item.objects.select_related('supplier').filter(supplier__visibility=True)
-        # queryset = models.Item.objects.filter(supplier__visibility=True)
-#        serializer = serializers.ItemSerializer(queryset, many=True)
+#        # queryset = models.Item.objects.filter(supplier__visibility=True)
+#        serializer = serializers.GetItemSerializer(queryset, many=True)
 #        return Response(serializer.data)
 
 
